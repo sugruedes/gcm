172b3076184305f7fc705b38b814568a4ddb5735
Des Sugrue <des@chainstor.org>
2015-11-03 01:41:41 +0000

    Initial commit.

---
+++ cp_semd.php
@@ -0,0 +1,19 @@
+<?php
+
+include 'ChatProtoPushMessage.php';
+
+$api_key = "APIKEY000";
+$url = "http://test.des.gcm/registered.php";
+$devices = array( "DEVICE_ID3");
+
+$message = "Using php file";
+
+
+$an = new ChatProtoPushMessage($api_key, $url);
+$an->setDevices($devices);
+
+$response = $an->send($message);
+
+
+
+?>
